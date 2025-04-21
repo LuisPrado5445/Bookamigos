662aaff142c469f83183e92807393070b365fef9
Luis Angel Prado <luisprado11b@gmail.com>
2025-04-20 19:34:39 -0500

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -10,10 +10,17 @@ app.use(cors());
 app.use(express.json());
 
 // Rutas
-app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/auth', require('./routes/authRoutes')); // quitado el espacio después de '/api/auth'
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/reviews', require('./routes/reviewRoutes'));
 app.use('/api/books', require('./routes/bookRoutes'));
+app.use('/api/discusiones', require('./routes/discussionRoutes'));
+app.use('/api/follows', require('./routes/followRoutes')); // ✅ Nueva ruta agregada
+
+// Ruta de prueba principal
+app.get('/', (req, res) => {
+  res.send('🚀 API Bookamigos funcionando correctamente');
+});
 
 app.listen(PORT, async () => {
   try {
@@ -23,8 +30,5 @@ app.listen(PORT, async () => {
   } catch (error) {
     console.error('Error al conectar a la base de datos:', error);
   }
-  app.get('/', (req, res) => {
-    res.send('🚀 API Bookamigos funcionando correctamente');
-  });
-  
 });
+
