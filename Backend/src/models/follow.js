bd627770815a52995ba844dc92cf6e93a160bc76
Luis Angel Prado <luisprado11b@gmail.com>
2025-04-20 19:40:57 -0500

    Create follow.js

---
+++ Backend/src/models/follow.js
@@ -0,0 +1,29 @@
+// src/models/follow.js
+module.exports = (sequelize, DataTypes) => {
+    const Follow = sequelize.define('Follow', {
+      seguidorId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      seguidoId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      }
+    }, {
+      timestamps: true,
+      indexes: [
+        {
+          unique: true,
+          fields: ['seguidorId', 'seguidoId']
+        }
+      ]
+    });
+  
+    Follow.associate = (db) => {
+      db.Follow.belongsTo(db.User, { as: 'seguidor', foreignKey: 'seguidorId' });
+      db.Follow.belongsTo(db.User, { as: 'seguido', foreignKey: 'seguidoId' });
+    };
+  
+    return Follow;
+  };
+  
