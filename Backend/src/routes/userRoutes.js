544d6b3f13125194be6eaffac5aa32f76d65431d
Luis Angel Prado <luisprado11b@gmail.com>
2025-04-20 19:38:46 -0500

    Update userRoutes.js

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const authenticateToken = require('../middleware/authMiddleware');
 
-router.get('/', authenticateToken, userController.getUsers); // Ruta protegida
+// Todas las rutas de usuarios requieren estar autenticado
+router.get('/', authenticateToken, userController.getUsers);
+router.get('/:id', authenticateToken, userController.getUserById);
+router.put('/:id', authenticateToken, userController.updateUser);
+router.delete('/:id', authenticateToken, userController.deleteUser);
 
 module.exports = router;
