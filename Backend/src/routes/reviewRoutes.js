1b6ed10fbb1e30dc259a72d97ec9633639f00615
Luis Angel Prado <luisprado11b@gmail.com>
2025-04-20 19:38:21 -0500

    Update reviewRoutes.js

--- Backend/src/routes/reviewRoutes.js
+++ Backend/src/routes/reviewRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/reviewController');
+const authenticateToken = require('../middleware/authMiddleware');
 
 router.get('/', reviewController.getReviews);
+router.post('/', authenticateToken, reviewController.createReview);
 
 module.exports = router;
